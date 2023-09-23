751d79c24bb939d8904c62005a478ae0575e6e01
Rudhi Bashambu <rudhib2@illinois.edu>
2023-09-23 17:11:42 -0500

    signals

--- shell/shell.c
+++ shell/shell.c
@@ -23,27 +23,10 @@
 #define MAX_INPUT_SIZE 1024
 static pid_t foreground_pid = 0;
 
-// extern char *optarg;
-
-
 typedef struct process {
     char *command;
     pid_t pid;
 } process;
-
-
-
-#include "format.h"
-#include "shell.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <string.h>
-#include <sys/types.h>
-#include <dirent.h>
-
-#define MAX_INPUT_SIZE 1024
 
 void sigint_handler(int signo) {
     if (foreground_pid > 0) {
@@ -62,46 +45,12 @@ int change_directory(const char* path) {
     return 0;
 }
 
-// int execute_command(char* command) {
-//     if (strcmp(command, "") == 0) {
-//         return 0;  // Empty command
-//     } else if (strncmp(command, "cd ", 3) == 0) {
-//         return change_directory(command + 3); // +3 to skip "cd "
-//     }
-
-//     int status;
-//     pid_t new_pid = fork();
-
-//     if (new_pid == 0) {
-//         char* token = strtok(command, " ");
-//         char* args[100];
-//         int i = 0;
-//         while (token != NULL) {
-//             args[i++] = token;
-//             token = strtok(NULL, " ");
-//         }
-//         args[i] = NULL;
-//         execvp(args[0], args);
-//         // perror("execvp failed");
-//         exit(0);
-//     } else if (new_pid > 0) {
-//         waitpid(new_pid, &status, 0);
-//         return WEXITSTATUS(status);
-//     } else {
-//         print_fork_failed();
-//         return -1;
-//     }
-//     return 0;
-// }
-
-
 int execute_command(char* command) {
     if (strcmp(command, "") == 0) {
         return 0;  // Empty command
     } else if (strncmp(command, "cd ", 3) == 0) {
         return change_directory(command + 3); // +3 to skip "cd "
     }
-
     int status;
     pid_t new_pid = fork();
 
@@ -109,8 +58,7 @@ int execute_command(char* command) {
         // Redirection variables
         char* output_file = NULL;
         char* input_file = NULL;
-
-        // Check for output redirection '>'
+        
         if (strstr(command, " > ")) {
             char* output_redirect = strstr(command, ">");
             if (output_redirect) {
@@ -120,22 +68,7 @@ int execute_command(char* command) {
                 dup2(fd, STDOUT_FILENO);
                 close(fd);
             }
-           
-         } 
-         //else if (strstr(command, " >> ")) {
-        //     // Check for append output redirection '>>'
-        //     char* append_redirect = strstr(command, ">>");
-        //     if (append_redirect) {
-        //         *append_redirect = '\0'; // Split the command
-        //         output_file = strtok(append_redirect + 2, " "); // Get the filename
-        //         int fd = open(output_file, O_WRONLY | O_CREAT | O_APPEND, 0644);
-        //         dup2(fd, STDOUT_FILENO);
-        //         close(fd);
-        //     }
-            
-        //} 
-        else if (strstr(command, " >> ")) {
-    // Check for append output redirection '>>'
+         } else if (strstr(command, " >> ")) {
             char* append_redirect = strstr(command, " >> ");
             if (append_redirect) {
                 *append_redirect = '\0'; // Split the command
@@ -144,8 +77,7 @@ int execute_command(char* command) {
                 dup2(fd, STDOUT_FILENO);
                 close(fd);
             }
-        }
-        else if (strstr(command, " < ")) {
+        } else if (strstr(command, " < ")) {
             char* input_redirect = strstr(command, "<");
             if (input_redirect) {
                 *input_redirect = '\0'; // Split the command
@@ -153,10 +85,8 @@ int execute_command(char* command) {
                 int fd = open(input_file, O_RDONLY);
                 dup2(fd, STDIN_FILENO);
                 close(fd);
-            }
-            
+            } 
         }
-
         char* token = strtok(command, " ");
         char* args[100];
         int i = 0;
@@ -180,7 +110,6 @@ int execute_command(char* command) {
 
 void execute_ps(char written_in_terminal[]) {
     print_process_info_header();
-
     process_info pinfo;
     pinfo.pid = getpid();
     pinfo.nthreads = 1;
@@ -191,20 +120,13 @@ void execute_ps(char written_in_terminal[]) {
     pinfo.command = written_in_terminal;
     printf("%s", written_in_terminal);
     print_process_info(&pinfo);
-
-    // Repeat the above process_info printing for other processes
-
-    // You can gather information about other processes and print them here
 }
-
-
 
 int execute_logical_command(char* logical_command) {
     char* token;
     char* command1;
     char* command2;
     int operator_type; // 0 for ;, 1 for &&, 2 for ||
-    
     if ((token = strstr(logical_command, "&&"))) {
         operator_type = 1;
         command1 = strtok(logical_command, "&&");
@@ -219,7 +141,6 @@ int execute_logical_command(char* logical_command) {
         command1 = strtok(logical_command, ";");
         command2 = token + 2;
     }
-
     int status1 = execute_command(command1);
 
     if (operator_type == 1) {
@@ -238,7 +159,6 @@ int execute_logical_command(char* logical_command) {
     return status1;
 }
 
-
 void execute_script(const char* filename) {
     FILE *script_file = fopen(filename, "r");
     if (script_file == NULL) {
@@ -246,12 +166,10 @@ void execute_script(const char* filename) {
         // perror("Error opening script file");
         exit(0);
     }
-
     char line[MAX_INPUT_SIZE];
 
     while (fgets(line, sizeof(line), script_file)) {
         line[strlen(line) - 1] = '\0';
-
         int status;
         pid_t new_pid = fork();
 
@@ -280,6 +198,47 @@ void execute_script(const char* filename) {
     fclose(script_file);
 }
 
+void handle_kill_command(char *command) {
+    int pid;
+    if (sscanf(command, "kill %d", &pid) == 1) {
+        int result = kill(pid, SIGKILL);
+        if (result == 0) {
+            print_killed_process(pid, command);
+        } else {
+            print_no_process_found(pid);
+        }
+    } else {
+        printf("kill was ran without a pid\n");
+    }
+}
+
+void handle_stop_command(char *command) {
+    int pid;
+    if (sscanf(command, "stop %d", &pid) == 1) {
+        int result = kill(pid, SIGSTOP);
+        if (result == 0) {
+            print_stopped_process(pid, command);
+        } else {
+            print_no_process_found(pid);
+        }
+    } else {
+        printf("stop was ran without a pid\n");
+    }
+}
+
+void handle_cont_command(char *command) {
+    int pid;
+    if (sscanf(command, "cont %d", &pid) == 1) {
+        int result = kill(pid, SIGCONT);
+        if (result == 0) {
+            print_continued_process(pid, command);
+        } else if (result == -1) {
+            print_no_process_found(pid);
+        }
+    } else {
+        printf("cont was ran without a pid\n");
+    }
+}
 
 int shell(int argc, char *argv[]) {
     vector *vect = string_vector_create();
@@ -287,7 +246,6 @@ int shell(int argc, char *argv[]) {
     char curr_dir[MAX_INPUT_SIZE];
     char written_in_terminal[MAX_INPUT_SIZE];
     pid_t pid = getpid();
-    
     int history_flag = 0;
     char* history_filename = NULL;
     if (argc == 3 && strcmp(argv[1], "-h") == 0) {
@@ -317,13 +275,6 @@ int shell(int argc, char *argv[]) {
         exit(0); // Exit after running the script
     }
 
-    // if (argc == 2 && strcmp(argv[1], "ps") == 0) {
-    //     printf("ANUSHK");
-    //     execute_ps();
-    //     printf("SNUSHKA");
-    //     return 0;  // Exit after running the ps command
-    // }
-
     while(1) {
         print_prompt(getcwd(curr_dir, sizeof(curr_dir)), pid);
     
@@ -336,7 +287,24 @@ int shell(int argc, char *argv[]) {
             }
         }
 
-        
+        // if((strstr(written_in_terminal, "!history")) == NULL && (strstr(written_in_terminal, "echo")) == NULL && 
+        // !strstr(written_in_terminal, "ls") && !strstr(written_in_terminal, "pwd") && !strstr(written_in_terminal, "cd ") && 
+        // !strstr(written_in_terminal, "-f ") && !strstr(written_in_terminal, "-h ") && !strstr(written_in_terminal, "^C") &&
+        // !strstr(written_in_terminal, "!") && !strstr(written_in_terminal, "#") && !strstr(written_in_terminal, "exit") && 
+        // !strstr(written_in_terminal, "^D") && strcmp(written_in_terminal, "") && !strstr(written_in_terminal, "sleep")) {
+        //     print_invalid_command(written_in_terminal);
+        // }
+
+        if (strstr(written_in_terminal, "kill ")) {
+            handle_kill_command(written_in_terminal);
+            continue; // Continue to the next iteration
+        } else if (strstr(written_in_terminal, "stop ")) {
+            handle_stop_command(written_in_terminal);
+            continue; // Continue to the next iteration
+        } else if (strstr(written_in_terminal, "cont ")) {
+            handle_cont_command(written_in_terminal);
+            continue; // Continue to the next iteration
+        }
 
         if (strstr(written_in_terminal, "#")) {
         size_t num = atoi(written_in_terminal + 1);
@@ -365,8 +333,6 @@ int shell(int argc, char *argv[]) {
                 exit(1);
             }
         }
-            
-
 
         if (strcmp(written_in_terminal, "!history") != 0) {
             vector_push_back(vect, written_in_terminal);
@@ -379,7 +345,6 @@ int shell(int argc, char *argv[]) {
         }
 
         int status;
-
         if (history_flag) {
             if (strcmp(written_in_terminal, "!history") != 0) {
                 fprintf(history_file, "\n%s", written_in_terminal);
@@ -400,20 +365,8 @@ int shell(int argc, char *argv[]) {
             for (size_t i = 0 ; i < vector_size(vect); ++i) {
                 print_history_line(i, vector_get(vect, i));
             }
-        }
-
-        // if((strstr(written_in_terminal, "!history")) == NULL && (strstr(written_in_terminal, "echo")) == NULL && 
-        // !strstr(written_in_terminal, "ls") && !strstr(written_in_terminal, "pwd") && !strstr(written_in_terminal, "cd ") && 
-        // !strstr(written_in_terminal, "-f ") && !strstr(written_in_terminal, "-h ") && !strstr(written_in_terminal, "^C") &&
-        // !strstr(written_in_terminal, "!") && !strstr(written_in_terminal, "#") && !strstr(written_in_terminal, "exit") && 
-        // !strstr(written_in_terminal, "^D") && strcmp(written_in_terminal, "") && !strstr(written_in_terminal, "sleep")) {
-        //     print_invalid_command(written_in_terminal);
-        // }
-
-        
-        
+        }        
     }
-    
     vector_destroy(vect);
     return 0;
 }
