2d7c7c6a1b75605c9f9b6d05fea6610ab3e67d14
Rudhi Bashambu <rudhib2@illinois.edu>
2023-10-16 23:27:20 -0500

    try

--- password_cracker/cracker1.c
+++ password_cracker/cracker1.c
@@ -1,95 +1,67 @@
+//used ChatGPT for initial design and debugging - worked well
 /**
  * password_cracker
  * CS 341 - Fall 2023
  */
+
 #include "cracker1.h"
 #include "format.h"
 #include "utils.h"
 #include "includes/queue.h"
-
 #include <stdio.h>
 #include <string.h>
 #include <crypt.h>
-
-// typedef struct craker_t {
-//     pthread_t tid;
-//     size_t index;
-// } cracker_t;
-
-
-// int start(size_t thread_count) {
-//     // TODO your code here, make sure to use thread_count!
-//     // Remember to ONLY crack passwords in other threads
-//     return 0; // DO NOT change the return code since AG uses it to check if your
-//               // program exited normally
-// }
-
-// pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
-// static int recoverd = 0;
-static queue *tasks = NULL;
-// static cracker_t *crackers = NULL;
-static int total = 0;
-
-
-#include "cracker1.h"
-#include "format.h"
-#include "utils.h"
 #define USERNAME_MAX_LEN 9
 #define HASH_MAX_LEN 14
 #define PASSWORD_MAX_LEN 32
 
-pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
-static int passwordsRecovered = 0;
-
-typedef struct {
-    pthread_t threadId;
-    size_t threadIndex;
-} CrackerThread;
-
+static queue *tasks = NULL;
+static int total = 0;
 static void* crackPassword(void* data);
 static void updateRecoveredCount();
 static int isPasswordFound(const char* encryptedPassword, const char* candidatePassword);
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+static int passwordsRecovered = 0;
 
 void* crackPassword(void* data) {
-    CrackerThread* cracker = (CrackerThread*)data;
     char* taskData = NULL;
+    int threadId = (long) data;
     char username[USERNAME_MAX_LEN];
     char storedHash[HASH_MAX_LEN];
     char candidatePassword[PASSWORD_MAX_LEN];
-
     while ((taskData = queue_pull(tasks)) != NULL) {
         sscanf(taskData, "%s %s %s", username, storedHash, candidatePassword);
-        v1_print_thread_start(cracker->threadIndex, username);
+        v1_print_thread_start(threadId, username);
         double startTime = getThreadCPUTime();
-        int prefixLength = getPrefixLength(candidatePassword);
+        double prefixLength = getPrefixLength(candidatePassword);
         int hashAttempts = 0;
         int passwordFound = 0;
-
-        if (prefixLength == (int)strlen(candidatePassword)) {
-            passwordFound = 1;
-        } else {
-            int suffixLength = strlen(candidatePassword) - prefixLength;
+        if (prefixLength != (int)strlen(candidatePassword)) {
             struct crypt_data cryptData;
-            cryptData.initialized = 0;
-            setStringPosition(candidatePassword + prefixLength, 0);
+            setStringPosition(candidatePassword + (int)prefixLength, 0);
             int totalVariations = 1;
-            for (int i = 0; i < suffixLength; ++i) {
+            // brute force check all ways
+            for (int i = 0; i < (int)strlen(candidatePassword) - prefixLength; ++i) {
                 totalVariations *= 26;
-            }
-            for (int i = 0; i < totalVariations; ++i) {
-                ++hashAttempts;
-                if (isPasswordFound(crypt_r(candidatePassword, "xx", &cryptData), storedHash)) {
-                    updateRecoveredCount();
-                    passwordFound = 1;
-                    break;
+                if(i == ((int)strlen(candidatePassword) - prefixLength-1)){
+                    for (int i = 0; i < totalVariations; ++i) {
+                        hashAttempts++;
+                        if (isPasswordFound(crypt_r(candidatePassword, "xx", &cryptData), storedHash)) {
+                            updateRecoveredCount();
+                            passwordFound = 1;
+                            break;
+                        }
+                        incrementString(candidatePassword);
+                    }
                 }
-                incrementString(candidatePassword);
             }
+        } else{
+            passwordFound = 0;
         }
-        v1_print_thread_result(cracker->threadIndex, username, candidatePassword, 
+         v1_print_thread_result(threadId, username, candidatePassword, 
             hashAttempts, getThreadCPUTime() - startTime, !passwordFound);
-        free(taskData);
-        taskData = NULL;
+            free(taskData);
+            taskData = NULL;
     }
     queue_push(tasks, NULL);
     free(taskData);
@@ -108,14 +80,12 @@ int isPasswordFound(const char* encryptedPassword, const char* storedHash) {
 
 int start(size_t threadCount) {
     tasks = queue_create(0);
-    CrackerThread* threads = malloc(threadCount * sizeof(CrackerThread));
-    for (size_t i = 0; i < threadCount; ++i) {
-        threads[i].threadIndex = i + 1;
-        pthread_create(&(threads[i].threadId), NULL, crackPassword, &(threads[i]));
+    pthread_t tid[threadCount];
+    for (size_t i = 0; i < threadCount; i++) {
+      pthread_create(tid+i, NULL, crackPassword, (void *)(i+1));
     }
     char* line = NULL;
     size_t size = 0;
-
     while (getline(&line, &size, stdin) != -1) {
         queue_push(tasks, strdup(line));
         ++total;
@@ -123,10 +93,9 @@ int start(size_t threadCount) {
     queue_push(tasks, NULL);
     free(line);
     for (size_t i = 0; i < threadCount; ++i) {
-        pthread_join(threads[i].threadId, NULL);
+        pthread_join(tid[i], NULL);
     }
     v1_print_summary(passwordsRecovered, total - passwordsRecovered);
-    free(threads);
     queue_destroy(tasks);
     return 0;
 }
